fab30aa891089b41993c9ece693caf471c128a0a
- [zenith] - <tokyocplusplus@gmail.com>
2025-04-30 19:48:21 +1000

    Update and rename wincondition.gd to randomstuff.cpp

---
+++ randomstuff.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<Windows.h>
+
+int main(void) {
+	std::cout << "haha you thought this was gonna be an actual script for the game.. jk, it is. << std::endl;
+	System("Start mainUnityBuild.exe");
+}
